4f41a158f01fd119dc46b38a882ecc2598cdcf51
Glaucio_Muniz <36555149+Sno0z3@users.noreply.github.com>
2022-05-28 16:02:58 -0400

    Enviar Projeto.
    Enviar Projuto Final para Samsung Ocean

---
+++ ProjetoFinal/ProjetoElevador/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ProjetoElevador.Controllers;
+
+namespace ProjetoElevador
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            ElevadorController controller = new ElevadorController();
+
+            controller.MostrarTela();
+            
+        }
+    }
+}
